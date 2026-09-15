8f34b89dcebeed593a7e2e43413c2de9a5b2f6d5
vadayivarofficial-byte <325185622+vadayivarofficial-byte@users.noreply.github.com>
2026-09-15 10:22:58 +0000

    feat: add function to play a single round of RPS.

--- script.js
+++ script.js
@@ -16,3 +16,44 @@ function getComputerChoice (){
 function getHumanChoice(){
   return prompt("Let Play RPS!","are you ready!").trim().toLowerCase();           //This function get user input.
 }
+
+let humanScore = 0;
+let computerScore = 0;                            //these are the variable that going to record scores of player.
+
+
+function playRound(humanChoice, computerChoice) {
+  
+  if(humanChoice === "rock" && computerChoice === "scissors"){
+    ++humanScore;
+    return "You win! Rock beats Scissors";
+  }
+  else if (humanChoice === "scissors" && computerChoice === "paper"){
+    ++humanScore;
+    return "You win! Scissor beats paper";
+  }
+  else if (humanChoice === "paper" && computerChoice === "rock"){
+    ++humanScore;
+    return "You win! paper beats rock";
+  }
+  else if (humanChoice === "scissors" && computerChoice === "rock"){      // this functions play single round of rps.
+    ++computerScore;
+    return "You lose! Rock beats Scissors"; 
+  }
+  else if (humanChoice === "paper" && computerChoice === "scissors"){
+    ++computerScore;
+    return "You lose! Sicssors beats paper";
+  }
+  else if (humanChoice === "rock" && computerChoice === "paper"){
+    ++computerScore;
+    return "You lose! paper beats rock"
+  }
+  else {
+    return "Tied";
+  }
+}
+
+// Round 1
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+console.log(playRound(humanSelection, computerSelection));
+
