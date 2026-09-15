66834d2bfa876afccc4e0c285059470f95424c26
vadayivarofficial-byte <325185622+vadayivarofficial-byte@users.noreply.github.com>
2026-09-15 09:36:37 +0000

    feat: add function to get user input

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function getComputerChoice (){
   let random = Math.random();
 
   if(random < 0.33){
-    return "rock";
+    return "rock";                                  //This function generates random "rock", "paper" and scissors Choice.
   }
   else if (random < 0.66){
     return "paper";
@@ -10,4 +10,9 @@ function getComputerChoice (){
   else{
     return "scissors";
   }
+}
+
+
+function getHumanChoice(){
+  return prompt("Let Play RPS!","are you ready!").trim().toLowerCase();           //This function get user input.
 }
